becdf6a9290d66d33cd868537a5ad7ccaf295880
silence <schibarara@gmail.com>
2020-09-26 10:39:27 +0200

    Start Phone Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -26,9 +26,7 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
+
     public void createMapIntent(View view){
 Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW,uri);
@@ -44,4 +42,11 @@ Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        Uri uri = Uri.parse("tel:0123456789");
+        intent.setData(uri);
+        startActivity(intent);
+    }
 }
